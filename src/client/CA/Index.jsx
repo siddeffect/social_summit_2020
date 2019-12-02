c002c3410442af39299687afec1278addaaabfe8
IamArnab01 <imarnab01@gmail.com>
2019-12-02 21:27:29 +0530

    Why CA page created

--- src/client/CA/Index.jsx
+++ src/client/CA/Index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import RoleCA from "./RoleCA";
-// import WhyCA from "./WhyCA";
+import WhyCA from "./WhyCA";
 import CABackground from "./CABackground";
 
 export class Index extends Component {
@@ -8,7 +8,7 @@ export class Index extends Component {
     return (
       <div>
         <CABackground></CABackground>
-        {/* <WhyCA></WhyCA> */}
+        <WhyCA></WhyCA>
         {/* <RoleCA> </RoleCA> */}
       </div>
     );
---
+++ src/client/CA/WhyCASub.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import styles from "./css/WhyCA.module.css";
+import PropTypes from "prop-types";
+
+export class WhyCASub extends Component {
+  render() {
+    const paraText = this.props.content.map((item, index) => {
+      return (
+        <p
+          key={index}
+          style={this.props.style ? { whiteSpace: this.props.style } : null}
+          className={styles.reasonsText}
+        >
+          {item.text}
+        </p>
+      );
+    });
+    return (
+      <div className={styles.imgTextWrapper}>
+        <p className={styles.reasonsHeading}>{this.props.heading}</p>
+        <img alt={this.props.heading} src={this.props.src}></img>
+        {paraText}
+      </div>
+    );
+  }
+}
+
+export default WhyCASub;
+
+WhyCASub.propTypes = {
+  heading: PropTypes.string,
+  src: PropTypes.string,
+  content: PropTypes.string,
+  style: PropTypes.string
+};
