4d12a5b235af74e5d84cf26231fdb58184f1be2b
Siddharth Varshney <siddharth.98varshney@gmail.com>
2019-12-21 00:21:03 +0530

    add new navbar

--- src/client/CA/auth/SignUp.js
+++ src/client/CA/auth/SignUp.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { signUp } from "../../../store/actions/authAction";
-import Navbar from "../layout/Navbar";
+import Navbar from "../NewLayout/Navbar";
 import M from "materialize-css";
 
 class SignUp extends Component {
