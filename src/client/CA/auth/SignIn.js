0c48778a3a1d7e9df1b883793a0ffe01d2947c5e
Siddharth Varshney <siddharth.98varshney@gmail.com>
2019-12-22 02:40:57 +0530

    signin mui done

--- src/client/CA/auth/SignIn.js
+++ src/client/CA/auth/SignIn.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import {
+  withStyles,
+  ThemeProvider,
+  createMuiTheme
+} from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { signIn } from "../../../store/actions/authAction";
 import { Redirect } from "react-router-dom";
 import Navbar from "../CALayout/Navbar";
+import { Grid, TextField, Button, Hidden } from "@material-ui/core";
+
+const styles = theme => ({
+  formHeader: {
+    color: "#212121",
+    fontSize: "3rem",
+    marginTop: "10vh"
+  },
+  textField: {
+    width: "25vw",
+    marginBottom: "4vh"
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: "#00a650" }
+  }
+});
 
 class SignIn extends Component {
   state = {
@@ -20,50 +44,76 @@ class SignIn extends Component {
     e.preventDefault();
     this.props.signIn(this.state);
   };
+
   render() {
-    const { authError, auth } = this.props;
+    const { classes, authError, auth } = this.props;
     if (auth.uid) return <Redirect to="/campusambassador/dashboard" />;
 
     return (
       <div style={{ background: "#FFF6E3" }}>
-        <Navbar style={{ margin: "10vh 0 20vh 0" }} />
-        <div className="container">
-          <form onSubmit={this.handleSubmit} style={{ background: "#FFF6E3" }}>
-            <h3 className="grey-text text-darken-3 center-align">Sign In</h3>
-            <div className="row valign-wrapper">
-              <div className="col l5 m5 s12">
-                <div className="input-field">
-                  <label htmlFor="email">Email</label>
-                  <input type="email" id="email" onChange={this.handleChange} />
-                </div>
-                <div className="input-field">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    id="password"
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div className="input-field center-align">
-                  <button
-                    className="btn-large #409A4B "
-                    type="submit"
-                    name="action"
-                  >
-                    Login
-                  </button>
-                  <div className="red-text center">
+        <ThemeProvider theme={theme}>
+          <Navbar style={{ margin: "10vh 0 20vh 0" }} />
+          <Grid container spacing={3} justify="center">
+            <Grid container xs={12} justify="center">
+              <label className={classes.formHeader}>Sign In</label>
+            </Grid>
+            <Grid item xs={6} alignContent="center" container justify="center">
+              <form
+                onSubmit={this.handleSubmit}
+                style={{ background: "#FFF6E3" }}
+              >
+                <div className={classes.margin}>
+                  <Grid container spacing={1}>
+                    <TextField
+                      id="email"
+                      label="Email"
+                      type="email"
+                      className={classes.textField}
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                  <Grid container spacing={1}>
+                    <TextField
+                      id="password"
+                      label="Password"
+                      type="password"
+                      className={classes.textField}
+                      autoComplete="current-password"
+                      onChange={this.handleChange}
+                    />
+                  </Grid>
+                  <Grid container spacing={1} justify="center">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      size="large"
+                      className={classes.button}
+                    >
+                      Login
+                    </Button>
+                  </Grid>
+                  <Grid container spacing={1}>
                     {authError ? <p>{authError}</p> : null}
-                  </div>
+                  </Grid>
                 </div>
-              </div>
-
-              <div className="col hide-on-small-only l7 m7">
-                <img src="/Images/ca_hero_form.png" alt="ca_image" />
-              </div>
-            </div>
-          </form>
-        </div>
+              </form>
+            </Grid>
+            <Hidden mdDown>
+              <Grid
+                item
+                xs={6}
+                alignContent="center"
+                container
+                justify="center"
+              >
+                <div className="col hide-on-small-only l7 m7">
+                  <img src="/Images/ca_hero_form.png" alt="ca_image" />
+                </div>
+              </Grid>
+            </Hidden>
+          </Grid>
+        </ThemeProvider>
       </div>
     );
   }
@@ -79,4 +129,8 @@ const mapDispatchToProps = dispatch => {
     signIn: creds => dispatch(signIn(creds))
   };
 };
-export default connect(mapStateToProp, mapDispatchToProps)(SignIn);
+
+export default connect(
+  mapStateToProp,
+  mapDispatchToProps
+)(withStyles(styles)(SignIn));
