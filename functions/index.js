5320bce013a3fc3120125cd6aac2191eac36565b
Siddharth Varshney <siddharth.98varshney@gmail.com>
2019-12-31 02:33:51 +0530

    add firebase_function

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+exports.getLatestPosts = functions.https.onRequest(async (req, res) => {
+  const snapshot = await firestore.collection("caData").get();
+  res.send(snapshot.docs.map(doc => doc.data()));
+});
