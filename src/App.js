39e7e8b47c36c2b4d206165b433ae03c85f8ec11
Siddharth Varshney <siddharth.98varshney@gmail.com>
2020-01-05 23:44:11 +0530

    add scroll restoration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./client/Home/Home";
 import ComingSoon from "./client/ComingSoon/ComingSoon";
 import PageNotFound from "./client/404/404";
@@ -6,7 +6,12 @@ import CAPage from "./client/CA/Index";
 import CARegistration from "./client/CA/auth/SignUp";
 import CALogin from "./client/CA/auth/SignIn";
 import CADashboard from "./client/CA/dashboard/Dashboard";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation
+} from "react-router-dom";
 import MUN from "./client/MUN/MUN";
 
 const Page404 = ({ location }) => (
@@ -18,50 +23,59 @@ const Page404 = ({ location }) => (
   </div>
 );
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/campusambassador">
-            <CAPage />
-          </Route>
-          <Route exact path="/campusambassador/signup">
-            <CARegistration />
-          </Route>
-          <Route exact path="/campusambassador/signin">
-            <CALogin />
-          </Route>
-          <Route exact path="/campusambassador/dashboard">
-            <CADashboard />
-          </Route>
-          <Route exact path="/mun">
-            <MUN />
-          </Route>
-          <Route exact path="/events">
-            <ComingSoon />
-          </Route>
-          <Route exact path="/about">
-            <ComingSoon />
-          </Route>
-          <Route exact path="/team">
-            <ComingSoon />
-          </Route>
-          <Route exact path="/contact">
-            <ComingSoon />
-          </Route>
-          <Route exact path="/partners">
-            <ComingSoon />
-          </Route>
-          <Route component={Page404} />
-        </Switch>
-      </div>
+      <ScrollToTop />
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/campusambassador">
+          <CAPage />
+        </Route>
+        <Route exact path="/campusambassador/signup">
+          <CARegistration />
+        </Route>
+        <Route exact path="/campusambassador/signin">
+          <CALogin />
+        </Route>
+        <Route exact path="/campusambassador/dashboard">
+          <CADashboard />
+        </Route>
+        <Route exact path="/mun">
+          <MUN />
+        </Route>
+        <Route exact path="/events">
+          <ComingSoon />
+        </Route>
+        <Route exact path="/about">
+          <ComingSoon />
+        </Route>
+        <Route exact path="/team">
+          <ComingSoon />
+        </Route>
+        <Route exact path="/contact">
+          <ComingSoon />
+        </Route>
+        <Route exact path="/partners">
+          <ComingSoon />
+        </Route>
+        <Route component={Page404} />
+      </Switch>
     </Router>
   );
 }
