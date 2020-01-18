a758dd1a80eab8e2b6def1aaa8e40f3430ef7b2b
Siddharth Varshney <siddharth.98varshney@gmail.com>
2020-01-19 02:06:28 +0530

    add more events

--- src/client/Events/events_data.js
+++ src/client/Events/events_data.js
@@ -146,6 +146,130 @@ const eventData = {
       "https://docs.google.com/forms/d/e/1FAIpQLSfkl_yigbIlLcXI8NdkbpnjhVG5UpSXSCMJGjWqNrPb_IJzcA/viewform?usp=sf_link",
     registrationDeadLine: "7th Feb. 2020"
   },
+  workshop: {
+    name: "Workshop",
+    prize: "---",
+    description:
+      'National Social Summit brings to you, a series of enthralling workshops, which will enrich your pool of knowledge and provide you a deeper insight to the various social and environmental issues. In collaboration with Design Innovation Centre, IIT Roorkee, workshops on "Design Thinking and Innovation", "Challenges in Himalayan Region", & "Innovations for Clean India", will be organized, inspiring the delegates to think and create solutions.',
+    heldDate: "To be anounced",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1OQNJiAaCCQGtCIHE4IHFbGRAaEoU_ejb",
+    perks: ["Certificate", "Kits", "Refreshments"],
+    coordinator: [
+      {
+        name: "Shrey Shukla",
+        contact: "6388086335",
+        email: "sshukla.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3UTBd04Kvg60W6-f76Ix-dHJjzdCKQPzQh7j7JBU-HajMYg/viewform?usp=sf_link",
+    registrationDeadLine: "7th Feb. 2020"
+  },
+  inquizition: {
+    name: "Inquizition",
+    prizeWorth: "₹ 1000/- each",
+    description:
+      'Quizzing gives direction to all this amassed knowledge and purges the urge to test one’s mettle against another through the competitive spirit. Social Summit brings to you a series of quizzes to not just sate this innate desire but also to add to this ever-growing pool of facts in your head. Three independent online quizzes will be held on "The Diversity of Life", "Environmental rule of Law", & "Clean Energy Technology".',
+    heldDate: "26th Feb 9:00, 2nd Feb 9:00, 9th Feb 9:00",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1GdkIVoacdHQqBLvCNcDFMgUUk_8eY7XI",
+    perks: ["Certificate for winners"],
+    coordinator: [
+      {
+        name: "Aditi Singhal",
+        contact: "7726099123",
+        email: "aditi.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://dare2compete.com/o/inquizitionquiz-national-social-summit-20-indian-institute-of-technology-iit-roorkee-98504",
+    registrationDeadLine: "26th Feb., 2nd Feb., 9th Feb. 2020"
+  },
+  microfiction: {
+    name: "Microfiction",
+    prizeWorth: "₹ 1000",
+    description:
+      "Every pulse of a writer’s soul, every encounter of his life, every thought of his mind is personified in his works. Social Summit brings to you, Microfiction, an excellent shot for you to write short but strong stories or poems on social or environmental issues under 50 words, which are quick to read and hard to forget.",
+    heldDate: "till 15th Feb, 8 PM",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1aFX-6WsabyvUgJX3NRaRWUjwJ7R01oR3",
+    perks: ["Certificate for winners"],
+    coordinator: [
+      {
+        name: "Aditi Singhal",
+        contact: "7726099123",
+        email: "aditi.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://dare2compete.com/o/micro-fiction-short-stories-or-poems-national-social-summit-20-indian-institute-of-technology-iit-roorkee-98204",
+    registrationDeadLine: "31st Jan. 2020"
+  },
+  caricature: {
+    name: "Caricature",
+    prizeWorth: "₹ 1000",
+    description:
+      "Are you passionate about entrancing people in the mystique of your stories? Do characters come alive in your mind, flourishing under the light of creativity and creating a thrilling reality of your own? Social Summit presents Caricature, a compic strip competition, put your heads together over an intriguing comic strip to create enthralling stories.",
+    heldDate: "till 15th Feb, 8 PM",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1DVGBo6Smdft4s4kXmFOw0H2mqwMjCbCJ",
+    perks: ["Certificate for winners"],
+    coordinator: [
+      {
+        name: "Aditi Singhal",
+        contact: "7726099123",
+        email: "aditi.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://dare2compete.com/o/caricature-national-social-summit-20-indian-institute-of-technology-iit-roorkee-98200",
+    registrationDeadLine: "26th Feb., 2nd Feb., 9th Feb. 2020"
+  },
+  pixels: {
+    name: "Pixels",
+    prizeWorth: "₹ 1000",
+    description:
+      "An online photography competition where the participants need to capture on aspect of the changing world in an image. The picture should depict evolution of the society, be it better or worse.",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1tTIA9KCwC8z5RMgD-AbnIWVDARDS3iz4",
+    perks: ["Certificate for winners"],
+    coordinator: [
+      {
+        name: "Aditi Singhal",
+        contact: "7726099123",
+        email: "aditi.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://dare2compete.com/o/pixels-photography-national-social-summit-20-indian-institute-of-technology-iit-roorkee-98209",
+    registrationDeadLine: "26th Feb., 2nd Feb., 9th Feb. 2020"
+  },
+  videoMaking: {
+    name: "1080p",
+    prizeWorth: "₹ 1000",
+    description:
+      "Social Summit provides you an ideal platform to showcase your ideas in the form of 1080p, the online video making competition.The subject matter of the video can be raising awareness regarding social or environmental issues.",
+    sponsoredBy: null,
+    ruleBook:
+      "https://drive.google.com/open?id=1rY58EbN8ly_32f26NA_CMevzNpPzj9kC",
+    perks: ["Certificate for winners"],
+    coordinator: [
+      {
+        name: "Aditi Singhal",
+        contact: "7726099123",
+        email: "aditi.socialsummit@gmail.com"
+      }
+    ],
+    registrationLink:
+      "https://dare2compete.com/o/1080p-video-making-national-social-summit-20-indian-institute-of-technology-iit-roorkee-98195",
+    registrationDeadLine: "26th Feb., 2nd Feb., 9th Feb. 2020"
+  },
   hackathon: {
     name: "Sociothon",
     prizeWorth: "₹ 20,000"
