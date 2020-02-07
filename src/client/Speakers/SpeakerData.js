4a8b1c27634e69f4f80a2c0c22b7ea4eb0a60900
Siddharth Varshney <siddharth.98varshney@gmail.com>
2020-02-08 02:36:52 +0530

    complete speaker grid

--- src/client/Speakers/SpeakerData.js
+++ src/client/Speakers/SpeakerData.js
@@ -1,12 +1,12 @@
-const speakerData = {
-  sonamWangchuk: {
+const speakerData = [
+  {
     name: "Sonam Wangchuk",
-    designation: "Founding director",
+    designation: "Founding Director",
     shortDesignation:
       "The Students' Educational and Cultural Movement of Ladakh",
     description:
       "Mr. Sonam Wangchuk is an Indian Engineer, Innovator and Education reformist. The Ramon Magsaysay Awardee is the founding director of SECMOL & continuously displays courageous service to the people and pragmatic idealism within a democratic society. Social reformist is acclaimed for his invention of Ice Stupa and contributions in launching Operation New Hope.",
-    photoRoute: "",
+    photoRoute: "Images/Speakers/sonam_wangchu-min.jpg",
     socialHandleLink: {
       wikipedia: "https://en.wikipedia.org/wiki/Sonam_Wangchuk_(engineer)",
       twitter: "https://twitter.com/Wangchuk66",
@@ -15,13 +15,13 @@ const speakerData = {
       instagram: null
     }
   },
-  shwetaKothari: {
+  {
     name: "Shweta Kothari",
     designation: "Managing Editor",
     shortDesignation: "The logical Indian",
     description:
       "Ms. Shweta Kothari is a journalist and a news presenter. She is currently the Managing Editor at The Logical Indian. Shweta has previously worked with NewsX and CNBC-TV18. She is a postgraduate in Journalism from the University of Sussex, UK. Shweta is a TEDx speaker and identifies herself as a feminist.",
-    photoRoute: "",
+    photoRoute: "Images/Speakers/shweta-min.jpg",
     socialHandleLink: {
       facebook: "https://www.facebook.com/shweta.kothari.18",
       instagram: "https://www.instagram.com/shwkothari/",
@@ -30,13 +30,13 @@ const speakerData = {
       wikipedia: null
     }
   },
-  lalitPande: {
+  {
     name: "Lalit Pande",
     designation: "Founder",
     shortDesignation: "Uttarakhand Seva Nidhi Paryavaran Shiksha Sansthan",
     description:
       "Dr. Lalit Pande is the founder of Uttarakhand Seva Nidhi Paryavaran Shiksha Sansthan and an environmentalist promoting environmental education in the hilly regions. After being a graduate from MIT and IIT, the Padma Shree Awardee reinstated his path to nature to introduce community educational programmes focused on environment and development.",
-    photoRoute: "",
+    photoRoute: "Images/Speakers/lalit_pandey-min.jpg",
     socialHandleLink: {
       wikipedia: "https://en.wikipedia.org/wiki/Lalit_Pande",
       linkedin: "https://www.linkedin.com/in/lalit-pande-538a3311/",
@@ -45,13 +45,13 @@ const speakerData = {
       twitter: null
     }
   },
-  sanjivChaturvedi: {
+  {
     name: "Sanjiv Chaturvedi",
     designation: "Conservator of Forest (Research)",
     shortDesignation: "IFS officer",
     description:
       "Mr. Sanjiv Chaturvedi is an Indian Forest Service officer (IFS) and also served as a Chief Vigilance Officer (CVO) at AIIMS. The dignitary was instrumental in the management of Corbett Tiger Reserve and undertook various conservation research projects in the state of Uttarakhand.",
-    photoRoute: "",
+    photoRoute: "Images/Speakers/chaturvedi-min.jpg",
     socialHandleLink: {
       wikipedia: "https://en.wikipedia.org/wiki/Sanjiv_Chaturvedi",
       facebook: null,
@@ -60,12 +60,13 @@ const speakerData = {
       twitter: null
     }
   },
-  abhishekJain: {
+  {
     name: "Abhishek Jain",
     designation: "Senior Programme",
     shortDesignation: "The Council on Energy, Environment and Water",
     description:
       "Mr. Abhishek is a Senior Programme Lead at The Council of Energy, environment and water. He co-conceptualised and leads CEEW’s flagship research effort, Access to Clean Cooking Energy and Electricity – Survey of States (ACCESS), the largest panel survey on multidimensional energy access in India.",
+    photoRoute: "Images/Speakers/abhishek_jain-min.jpg",
     socialHandleLink: {
       linkedin: "https://www.linkedin.com/in/abhishek1526/",
       twitter: "https://twitter.com/ajainme",
@@ -74,4 +75,6 @@ const speakerData = {
       instagram: null
     }
   }
-};
+];
+
+export default speakerData;
