23c765bc24c58f75f5ce70466ebbe24deed7b14c
Siddharth Varshney <siddharth.98varshney@gmail.com>
2020-02-07 19:06:39 +0530

    add speaker data

---
+++ src/client/Speakers/SpeakerData.js
@@ -0,0 +1,77 @@
+const speakerData = {
+  sonamWangchuk: {
+    name: "Sonam Wangchuk",
+    designation: "Founding director",
+    shortDesignation:
+      "The Students' Educational and Cultural Movement of Ladakh",
+    description:
+      "Mr. Sonam Wangchuk is an Indian Engineer, Innovator and Education reformist. The Ramon Magsaysay Awardee is the founding director of SECMOL & continuously displays courageous service to the people and pragmatic idealism within a democratic society. Social reformist is acclaimed for his invention of Ice Stupa and contributions in launching Operation New Hope.",
+    photoRoute: "",
+    socialHandleLink: {
+      wikipedia: "https://en.wikipedia.org/wiki/Sonam_Wangchuk_(engineer)",
+      twitter: "https://twitter.com/Wangchuk66",
+      facebook: null,
+      linkedin: null,
+      instagram: null
+    }
+  },
+  shwetaKothari: {
+    name: "Shweta Kothari",
+    designation: "Managing Editor",
+    shortDesignation: "The logical Indian",
+    description:
+      "Ms. Shweta Kothari is a journalist and a news presenter. She is currently the Managing Editor at The Logical Indian. Shweta has previously worked with NewsX and CNBC-TV18. She is a postgraduate in Journalism from the University of Sussex, UK. Shweta is a TEDx speaker and identifies herself as a feminist.",
+    photoRoute: "",
+    socialHandleLink: {
+      facebook: "https://www.facebook.com/shweta.kothari.18",
+      instagram: "https://www.instagram.com/shwkothari/",
+      twitter: "https://twitter.com/Shwkothari",
+      linkedin: "https://www.linkedin.com/in/shweta-kothari-b57a8453/",
+      wikipedia: null
+    }
+  },
+  lalitPande: {
+    name: "Lalit Pande",
+    designation: "Founder",
+    shortDesignation: "Uttarakhand Seva Nidhi Paryavaran Shiksha Sansthan",
+    description:
+      "Dr. Lalit Pande is the founder of Uttarakhand Seva Nidhi Paryavaran Shiksha Sansthan and an environmentalist promoting environmental education in the hilly regions. After being a graduate from MIT and IIT, the Padma Shree Awardee reinstated his path to nature to introduce community educational programmes focused on environment and development.",
+    photoRoute: "",
+    socialHandleLink: {
+      wikipedia: "https://en.wikipedia.org/wiki/Lalit_Pande",
+      linkedin: "https://www.linkedin.com/in/lalit-pande-538a3311/",
+      facebook: null,
+      instagram: null,
+      twitter: null
+    }
+  },
+  sanjivChaturvedi: {
+    name: "Sanjiv Chaturvedi",
+    designation: "Conservator of Forest (Research)",
+    shortDesignation: "IFS officer",
+    description:
+      "Mr. Sanjiv Chaturvedi is an Indian Forest Service officer (IFS) and also served as a Chief Vigilance Officer (CVO) at AIIMS. The dignitary was instrumental in the management of Corbett Tiger Reserve and undertook various conservation research projects in the state of Uttarakhand.",
+    photoRoute: "",
+    socialHandleLink: {
+      wikipedia: "https://en.wikipedia.org/wiki/Sanjiv_Chaturvedi",
+      facebook: null,
+      linkedin: null,
+      instagram: null,
+      twitter: null
+    }
+  },
+  abhishekJain: {
+    name: "Abhishek Jain",
+    designation: "Senior Programme",
+    shortDesignation: "The Council on Energy, Environment and Water",
+    description:
+      "Mr. Abhishek is a Senior Programme Lead at The Council of Energy, environment and water. He co-conceptualised and leads CEEW’s flagship research effort, Access to Clean Cooking Energy and Electricity – Survey of States (ACCESS), the largest panel survey on multidimensional energy access in India.",
+    socialHandleLink: {
+      linkedin: "https://www.linkedin.com/in/abhishek1526/",
+      twitter: "https://twitter.com/ajainme",
+      facebook: null,
+      wikipedia: null,
+      instagram: null
+    }
+  }
+};
