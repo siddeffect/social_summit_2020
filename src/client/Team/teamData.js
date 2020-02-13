cc79712b3cc279e54749eb0c161ab9953c30dd47
Siddharth Varshney <siddharth.98varshney@gmail.com>
2020-02-14 02:20:28 +0530

    add photos to team page

--- src/client/Team/teamData.js
+++ src/client/Team/teamData.js
@@ -1,60 +1,60 @@
 const team = [
   {
-    header: "Convener",
+    header: "Convenor",
     array: [
       {
         name: "Yash Agarwal",
         post: "Convenor",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Yash.jpg",
         socialHandleLink: {
-          facebook: "https://www.facebook.com/",
-          linkedin: "https://www.facebook.com/",
-          instagram: "https://www.facebook.com/"
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Parag Mittal",
         post: "Convenor",
-        src: "./Images//Speakers/chaturvedi-min.jpg",
+        src: "./Images/team/Parag.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Co-Convener",
+    header: "Co-Convenor",
     array: [
       {
         name: "Akshay Meena",
         post: "Co-Convenor",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Akshay.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
-        name: "Ankit Gaeg",
+        name: "Ankit Garg",
         post: "Co-Convenor",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Ankit.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Sanskar Chordiya",
         post: "Co-Convenor",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Sanskar.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
@@ -65,277 +65,277 @@ const team = [
       {
         name: "Siddharth Varshney",
         post: "Technical Head",
-        src: "./Images//Speakers/chaturvedi-min.jpg",
+        src: "./Images/team/Siddharth.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Team Operations",
+    header: "team Operations",
     array: [
       {
         name: "Subhendu Biswas",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Subhendu.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Ayush Shrivastava",
         post: "Manager",
-        src: "./Images//Speakers/chaturvedi-min.jpg",
+        src: "./Images/team/Ayush.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Heeralal Yadav",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Hiralal.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Jitendra Kumar",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Jitendra.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Dixit Panwar",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Dixit.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Shyam Tiwari",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Shyam.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Team Events",
+    header: "team Events",
     array: [
       {
         name: "Yash Aggarwal",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Aggarwal.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Aditi Singhal",
         post: "Manager",
-        src: "./Images/Speakers/shweta-min.jpg",
+        src: "./Images/team/Aditi.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Shrey Shukla",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Shrey.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Ujjval Singh",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Ujjval.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Arpit Shrivastava",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Arpit.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Team Promotions",
+    header: "team Promotions",
     array: [
       {
         name: "Vibhu Chhawchharia",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Vibhu.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Ankit Ranjan",
         post: "Manager",
-        src: "./Images//Speakers/chaturvedi-min.jpg",
+        src: "./Images/team/Ranjan.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Gaurav Kasat",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Kasat.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Adarsh Dubey",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Dubey.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Rakshit Raj",
         post: "Manager",
-        src: "./Images//Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Rakshit.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Deepak Chauhan",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Deepak.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Team Sponsors",
+    header: "team Sponsors",
     array: [
       {
         name: "Jaskaran Sethi",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Jaskaran.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Enagandula Sindhu",
         post: "Manager",
-        src: "./Images/Speakers/shweta-min.jpg",
+        src: "./Images/team/Sindhu.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
 
       {
         name: "Mohammad Zaryab",
         post: "Manager",
-        src: "./Images/Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Zaryab.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Arjun Aryan",
         post: "Manager",
-        src: "./Images/Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Arjun.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Priyansh Garg",
         post: "Manager ",
-        src: "./Images/Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Priyansh.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
   },
   {
-    header: "Team Design",
+    header: "team Design",
     array: [
       {
         name: "Sankalp Jha",
         post: "Manager",
-        src: "./Images/Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Sankalp.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       },
       {
         name: "Mayank Parnami",
         post: "Manager ",
-        src: "./Images/Speakers/sonam_wangchu-min.jpg",
+        src: "./Images/team/Mayank.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
@@ -346,11 +346,11 @@ const team = [
       {
         name: "Arnab Das",
         post: "Manager",
-        src: "./Images/Speakers/abhishek_jain-min.jpg",
+        src: "./Images/team/Arnab.jpg",
         socialHandleLink: {
-          facebook: "",
-          linkedin: "",
-          instagram: ""
+          facebook: null,
+          linkedin: null,
+          instagram: null
         }
       }
     ]
